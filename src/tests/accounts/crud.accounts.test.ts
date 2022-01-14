921a9bc181270e7033e3e4e323cdd643855f96aa
Valerka <val.1887@yandex.ru>
2022-01-14 20:22:49 +0300

    last commit . отображение списком аккаунтов

--- src/tests/accounts/crud.accounts.test.ts
+++ src/tests/accounts/crud.accounts.test.ts
@@ -12,25 +12,28 @@ describe('courses-crud', () => {
     describe('get', () => {
 
         it('crud_accounts', async () => {
-            let res = await api.get('accounts?pageSize=1&page=1');
-            expect(res.data.items.length).toBe(1);
-            expect(res.data.items[0].id).toBe(1);
-            expect(res.data.items[0].login).toBe('Valera_QA');
 
-            res = await api.get('accounts?pageSize=1&page=2');
-            expect(res.data.items.length).toBe(1);
+            let res = await api.get('users');
+            console.log(res.data.items)
+
+            expect(res.data.items.length).toBe(2);
             expect(res.data.items[0].id).toBe(2);
             expect(res.data.items[0].login).toBe('Serega_dev');
 
-            res = await api.get('accounts?pageSize=1&page=3');
-            expect(res.data.items.length).toBe(1);
-            expect(res.data.items[0].id).toBe(3);
-            expect(res.data.items[0].login).toBe('Danya_front');
+            expect(res.data.items[1].id).toBe(1);
+            expect(res.data.items[1].login).toBe('Valera_QA');
 
-            res = await api.get('accounts?pageSize=1&page=4');
-            expect(res.data.items.length).toBe(1);
-            expect(res.data.items[0].id).toBe(4);
-            expect(res.data.items[0].login).toBe('Kirill_UI_UX');
+
+
+            /* res = await api.get('accounts?pageSize=1&page=3');
+             expect(res.data.items.length).toBe(1);
+             expect(res.data.items[0].id).toBe(3);
+             expect(res.data.items[0].login).toBe('Danya_front');
+
+             res = await api.get('accounts?pageSize=1&page=4');
+             expect(res.data.items.length).toBe(1);
+             expect(res.data.items[0].id).toBe(4);
+             expect(res.data.items[0].login).toBe('Kirill_UI_UX');*/
         })
 
 

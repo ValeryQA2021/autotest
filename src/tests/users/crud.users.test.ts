4038b5dcc0a9e707b463396f7e24892ecb7edbfe
ValeryQA2021 <ilyenkov.valery@gmail.com>
2022-01-17 11:37:32 +0300

    update accounts. view all account in it

--- src/tests/users/crud.users.test.ts
+++ src/tests/users/crud.users.test.ts
@@ -14,9 +14,17 @@ describe('users-crud', () => {
         it('crud-users', async () => {
             let res = await api.get('users?pageSize=1&page=1')
             expect(res.data.items.length).toBe(1);
-            expect(res.data.items[0].id).toBe(2);
+            expect(res.data.items[0].id).toBe(4);
 
             res = await api.get('users?pageSize=1&page=2')
+            expect(res.data.items.length).toBe(1);
+            expect(res.data.items[0].id).toBe(3);
+
+            res = await api.get('users?pageSize=1&page=3');
+            expect(res.data.items.length).toBe(1);
+            expect(res.data.items[0].id).toBe(2);
+
+            res = await api.get('users?pageSize=1&page=4');
             expect(res.data.items.length).toBe(1);
             expect(res.data.items[0].id).toBe(1);
         })
